93507ee00971d43d96c46c568c0a32778a8f9e36
Willian de Paula Araujo <10529473@mydbs.ie>
2020-03-03 09:51:36 +0000

    invest method created

--- portfolio.py
+++ portfolio.py
@@ -5,3 +5,14 @@ Created on Tue Mar  3 09:20:34 2020
 @author: 10529473
 """
 
+# REPO: https://github.com/10529473/portfolio.git
+
+class Portfolio:
+    __value = 0
+    __assets = {}
+    
+    def __init__(self,asset,quantity):
+        self.__assets[asset] = quantity
+    
+    def invest(self,asset,quantity):
+        self.__assets[asset] = self.__assets.setdefault(asset,0.0) + quantity
